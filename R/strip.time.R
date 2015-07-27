1fb8fada0dd4ff228e6d1db45619728c2afd5b53
coffeemuggler <mdietze@gfz-potsdam.de>
2015-07-27 12:25:41 +0200

    Default output unit changed to "POSIXct". Examples updated to new settings.

--- R/strip.time.R
+++ R/strip.time.R
@@ -4,10 +4,10 @@ structure(function(# Function to create time units from sac-files.
   data,
   ### object, data set to be processed. 
 
-  unit = "JD",
+  unit = "POSIXct",
   ### \code{Character} scalar, date format of the output, one out of 
   ### \code{"JD"} (Julian Day), \code{"day"}, \code{"hour"}, \code{"min"}, 
-  ### \code{"sec"}.
+  ### \code{"sec"} and \code{"POSIXct"}. Default is \code{"POSIXct"}.
 
   shift = 0
   ### \code{Numeric} scalar, time shift [sec].
@@ -19,7 +19,13 @@ structure(function(# Function to create time units from sac-files.
   for(i in 1:length(data)) {
     ## create time vector
     dt <- data[[i]]$dt
-    t.0 <- ifelse(length(t) == 0, 0, max(t) + dt)
+
+    if(length(t) == 0) {
+      t.0 <- 0
+    } else {
+      t.0 <- max(t) + dt
+    } 
+    
     t <- c(t, shift + seq(from = t.0 / dt, to = i * data[[i]]$N - 1) * dt)
   }  
   
@@ -52,11 +58,24 @@ structure(function(# Function to create time units from sac-files.
     t <- shift + seq(from = min(t),
                      to = max(t),
                      by = dt) / 60
-  }  else if(unit == "sec") {
+  } else if(unit == "sec") {
     shift <- data[[1]]$DATTIM$sec
     t <- shift + seq(from = min(t),
                      to = max(t),
                      by = dt)
+  }else if(unit == "POSIXct") {
+    t.0 <- paste(data[[1]]$DATTIM$yr, "-", 
+                 data[[1]]$DATTIM$mo, "-", 
+                 data[[1]]$DATTIM$dom, "-", 
+                 data[[1]]$DATTIM$hr, "-", 
+                 data[[1]]$DATTIM$mi, "-", 
+                 data[[1]]$DATTIM$sec + data[[1]]$DATTIM$msec / 1000,
+                 sep = "")
+    
+    t.0 <- strptime(x = t.0, 
+                    format = "%Y-%m-%d-%H-%M-%S", 
+                    tz = "UTC")
+    t <- t.0 + seq(from = 0, by = dt, length.out = length(t))
   }
   
   return(t)
@@ -72,8 +91,11 @@ structure(function(# Function to create time units from sac-files.
   data(sac, envir = environment())
   
   ## create time vector from data array
-  t <- strip.time(data = sac, unit = "hour")
+  t <- strip.time(data = sac)
   
-  ## check results (in minutes), should be 10 minutes
-  summary(t * 60)
+  ## check class of the time vector
+  class(t)[1] == "POSIXct"
+  
+  ## show summary of t
+  summary(t)
 })
