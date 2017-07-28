14181233412795be53bd41fec8904a1312ee53df
coffeemuggler <mdietze@gfz-potsdam.de>
2017-07-28 23:38:21 +0200

    signal_hilbert(): eseis object support implemented

--- R/signal_hilbert.R
+++ R/signal_hilbert.R
@@ -2,7 +2,8 @@
 #' 
 #' The function calculates the Hilbert transform of the input signal vector.
 #' 
-#' @param data \code{Numeric} vector or list of vectors, input signal vector.
+#' @param data \code{eseis} object, \code{numeric} vector or list of 
+#' objects, data set to be processed.
 #' 
 #' @return \code{Numeric} vector or list of vectors, Hilbert transform.
 #' @author Michael Dietze
@@ -25,7 +26,31 @@ signal_hilbert <- function(
     
     ## return output
     return(data_out)
+    
   } else {
+    
+    ## get start time
+    eseis_t_0 <- Sys.time()
+    
+    ## collect function arguments
+    eseis_arguments <- list(data = "")
+    
+    ## check if input object is of class eseis
+    if(class(data) == "eseis") {
+      
+      ## set eseis flag
+      eseis_class <- TRUE
+      
+      ## store initial object
+      eseis_data <- data
+      
+      ## extract signal vector
+      data <- eseis_data$signal
+    } else {
+      
+      ## set eseis flag
+      eseis_class <- FALSE
+    }
     
     ## get length of the data set
     n <- length(data)
@@ -45,6 +70,30 @@ signal_hilbert <- function(
     
     ## get Hilbert transform
     data_out <- fftw::IFFT(h * fftw::FFT(data))
+    
+    ## optionally rebuild eseis object
+    if(eseis_class == TRUE) {
+      
+      ## assign aggregated signal vector
+      eseis_data$signal <- data_out
+      
+      ## calculate function call duration
+      eseis_duration <- as.numeric(difftime(time1 = Sys.time(), 
+                                            time2 = eseis_t_0, 
+                                            units = "secs"))
+      
+      ## update object history
+      eseis_data$history[[length(eseis_data$history) + 1]] <- 
+        list(time = Sys.time(),
+             call = "signal_hilbert()",
+             arguments = eseis_arguments,
+             duration = eseis_duration)
+      names(eseis_data$history)[length(eseis_data$history)] <- 
+        as.character(length(eseis_data$history))
+      
+      ## assign eseis object to output data set
+      data_out <- eseis_data
+    }
     
     ## return hilbert data set
     return(invisible(data_out)) 
