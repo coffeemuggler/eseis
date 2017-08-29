153b4db9ec34142cb72e3b2e265b862cb1dbd872
coffeemuggler <mdietze@gfz-potsdam.de>
2017-08-29 10:53:17 +0200

    general package updates

--- R/eseis-package.R
+++ R/eseis-package.R
@@ -15,12 +15,13 @@
 #' @author Michael Dietze
 #' @keywords Environmental Seismology, signal processing
 #' @importFrom graphics image plot axis axis.POSIXct box mtext
-#' @importFrom stats ccf spec.taper spectrum filter spec.pgram spec.ar median nextn
+#' @importFrom stats ccf spec.taper spectrum filter spec.pgram spec.ar median nextn runif sd
 #' @importFrom methods as new
 #' @importFrom Rcpp evalCpp
 #' @importFrom IRISSeismic readMiniseedFile getNetwork getStation getSNCL
 #' @importFrom grDevices colorRampPalette dev.off jpeg
-#' @importFrom utils read.delim write.table read.table sessionInfo
+#' @importFrom utils read.delim write.table read.table sessionInfo download.file
+#' @importFrom XML xmlParse xmlToList
 #' @useDynLib eseis
 NULL
 
