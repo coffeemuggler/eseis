115a5d0e5a9e08936a568261eb695318c9de9712
coffeemuggler <mdietze@gfz-potsdam.de>
2018-05-31 20:11:00 +0200

    sp dependency kicked out

--- R/spatial_convert.R
+++ R/spatial_convert.R
@@ -2,8 +2,8 @@
 #' 
 #' Coordinates are converted between reference systems.
 #' 
-#' @param data \code{Numeric} matrix or data frame, x-, y-coordinates to 
-#' be converted.
+#' @param data \code{Numeric} vector of length two or data frame, 
+#' x-, y-coordinates to be converted.
 #' 
 #' @param from \code{Character} value, proj4 string of the current 
 #' reference system.
@@ -12,16 +12,32 @@
 #' reference system. 
 #' 
 #' @return \code{Numeric} data frame with converted coordinates.
+#' 
 #' @author Michael Dietze
+#' 
 #' @keywords eseis
+#' 
 #' @examples
 #' 
-#' xy <- cbind(13, 55)
-#' 
+#' ## create lat lon coordinates
+#' xy <- c(13, 55)
+#'
+#' ## define input coordinate system 
 #' proj_in <- "+proj=longlat + ellps=WGS84"
 #' 
+#' ## define output coordinate system 
 #' proj_out <- "+proj=utm +zone=32 +datum=WGS84"
 #' 
+#' ## convert coordinate pair
+#' spatial_convert(data = xy, 
+#'                 from = proj_in, 
+#'                 to = proj_out)
+#'                 
+#' ## define set of coordinates
+#' xy <- data.frame(x = c(10, 11),
+#'                  y = c(54, 55))
+#'                  
+#' ## convert set of coordinates
 #' spatial_convert(data = xy, 
 #'                 from = proj_in, 
 #'                 to = proj_out)
@@ -34,41 +50,37 @@ spatial_convert <- function(
 ) {
   
   ## check input data
-  if(ncol(data) != 2) {
+  if(class(data) == "data.frame") {
     
-    warning("Coordinate data must contain only 2 columns")
-    return(c(NA, NA))
+    if(ncol(data) != 2) {
+      
+      stop("Coordinate data must contain only 2 columns, x and y!")
+    } else {
+      
+      ## convert data to matrix
+      data <- as.matrix(data)
+    }
+  } else if(class(data) == "numeric" | class(data) == "atomic") {
+    
+    if(length(data) != 2) {
+      
+      stop("Coordinate data must contain only 2 values, x and y!")
+    } else {
+      
+      ## convert vector to data frame
+      data <- cbind(x = data[1],
+                         y = data[2])
+    }
+  } else {
+    
+    stop("Only data frames and vectors are supported as input data!")
   }
   
-  test_from <- try(sp::CRS(from), 
-                   silent = TRUE)
-  
-  if(class(test_from) == "try-error") {
-    
-    warning("Input reference system incorrect!")
-    return(data)
-  }
-
-  test_to <- try(sp::CRS(to), 
-                   silent = TRUE)
-  
-  if(class(test_to) == "try-error") {
-    
-    warning("Output reference system incorrect!")
-    return(data)
-  }
-  
-  ## convert coordinates to SpatialPoints object
-  data <- sp::SpatialPoints(data)
-  
-  ## assign CRS to SpatialPoints object
-  sp::proj4string(data) <- sp::CRS(projargs = from)
-  
-  ## convert CRS
-  data <-sp::spTransform(x = data, CRS = to)
+  ## project data set
+  data <- rgdal::project(data, proj_out)
   
   ## convert output to data frame
-  data <- as.data.frame(sp::coordinates(data))
+  data <- as.data.frame(data)
   
   ## return data frame
   return(data)
