6dc9a7d4822a18607ccef6c4909faf7aa72c2f72
coffeemuggler <mdietze@gfz-potsdam.de>
2019-09-09 15:23:51 +0200

    default parameter ranges changed, still not happy

--- inst/shiny/models/ui.R
+++ inst/shiny/models/ui.R
@@ -22,24 +22,24 @@ shinyUI(fluidPage(
       tabPanel("Sediment", 
                sidebarPanel(
                  sliderInput("d_s", "Grain-size D_50 (log10 m):",
-                             min = -5, max = 2, value = -2, step = 0.03),
+                             min = -5, max = 2, value = 0, step = 0.03),
                  sliderInput("s_s", "Grain-size stdv. (log10):",
                              min = 0.1, max = 4, value = 0.9),
                  sliderInput("r_s", "Sediment density (k/m³):",
                              min = 500, max = 5000, value = 2650),
                  sliderInput("q_s", "Sediment flux (kg/ms):",
-                             min = 0, max = 10, value = 2, step = 0.01)
+                             min = 0, max = 100, value = 2, step = 0.01)
                )
       ),
       tabPanel("River", sidebarPanel(
         sliderInput("h_w", "River depth (m):",
-                    min = 0.01, max = 4, value = 1),
+                    min = 0.01, max = 40, value = 10),
         sliderInput("w_w", "River width (m):",
                     min = 1, max = 500, value = 5, step = 1),
         sliderInput("a_w", "River gradient (radians):",
-                    min = 0.0001, max = 0.01, value = 0.0075),
+                    min = 0.0001, max = 0.1, value = 0.0075),
         sliderInput("r_0", "Distance to river (m):",
-                    min = 1, max = 1000, value = 5.5)
+                    min = 1, max = 100000, value = 20)
       )),
       tabPanel("Seismic", sidebarPanel(
         sliderInput("q_0", "Quality factor at f_0 (n.u.):",
