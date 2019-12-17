23f25f4d553d110552d3aefacaa56160947ea38c
coffeemuggler <mdietze@gfz-potsdam.de>
2019-12-17 10:44:23 +0100

    CRAN test notes fixes

--- R/eseis-package.R
+++ R/eseis-package.R
@@ -17,7 +17,7 @@
 #' @aliases eseis-package
 #' @docType package
 #' @author Michael Dietze
-#' @keywords Environmental seismology, Signal processing, Natural hazard, Geomorphology, Earth science
+#' @keywords package
 #' @importFrom graphics image plot plot.default axis axis.POSIXct box mtext
 #' @importFrom stats acf ccf spec.taper spectrum filter spec.pgram spec.ar median nextn runif sd quantile splinefun cor nls residuals
 #' @importFrom methods as new
